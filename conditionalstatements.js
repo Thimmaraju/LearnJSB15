a58628a2ef6980cf6532d817856e0db8d6508f40
Thimmaraju <thimmaraju418@gmail.com>
2023-09-13 10:50:30 +0530

    operators and conditional

---
+++ conditionalstatements.js
@@ -0,0 +1,111 @@
+// if
+// else
+// if else 
+// switch 
+
+
+// condition
+
+// {
+
+//    //100
+
+// }
+
+// if(condition){
+
+
+//     //100 lines of code
+// }
+
+
+if(6<7){
+
+     console.log("remove well")
+}
+
+else{
+  
+
+    // create a well'
+    //then remove well
+
+    console.log("Create well")
+    console.log("Remove well")
+
+}
+
+//=====================================
+
+// condition1 
+ 
+//   task1
+
+//   condition2 
+ 
+//   task2
+
+//   condition3
+ 
+//   task3
+
+
+let marks=46
+
+if (marks > 90 && marks <= 100) {
+    console.log("grade = 'A+'");
+}
+else if (marks > 80 && marks <= 90) {
+    console.log("grade = 'A'");
+}
+else if (marks > 70 && marks <= 80) {
+    console.log("grade = 'B'");
+}
+else if (marks > 60 && marks <= 70) {
+    console.log("grade = 'C'");
+}
+else if (marks > 50 && marks <= 60) {
+    console.log("grade = 'D'");
+}
+else {
+    console.log("grade = 'fail'");
+}
+
+//Switch 
+
+d = new Date().getDay()
+
+console.log()
+
+
+
+let day;
+
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+ 
+  case  6:
+    day = "Saturday";
+  default:
+
+  day = "Raju"
+
+}
+
+console.log(day);
--- operators.js
+++ operators.js
@@ -69,3 +69,67 @@ console.log(val3)
 
  // == and ====
 
+
+ console.log(2=="2")
+
+ // == will compare only values not datatypes 
+
+ console.log(2==="2")
+
+  // === will compare not only values but also datatypes 
+
+//   AND 
+
+//   1  1  1
+
+//   1  0  0
+
+//   0  1  0
+
+//   0  0  0
+
+
+   console.log((4<5) && (3=="3"))
+
+//    OR 
+
+//    1 0  1
+
+//    0 1  1
+//    0 0  0
+//    1 1  1
+
+
+// NOT 
+
+// 1 0
+// 0 1
+
+console.log(!(3<5) )
+
+
+stuname = "Raju"
+
+num = 5
+
+num *= 5
+
+num = num *5
+
+
+console.log(num)
+
+stu1 = "Manju"
+
+stu2 = "Ranjitha"
+
+// condition
+
+// student=
+
+// Ternary
+// <condition> ? <value1> : <value2>;
+
+finalstu = 3=="3" ?  stu1 : stu2
+
+console.log(finalstu)
